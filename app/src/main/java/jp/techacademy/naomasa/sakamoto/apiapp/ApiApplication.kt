a2e0f375cb33ce2e894424f8770a25f42a3a2253
sakamona <sakamona@cocoto.co.jp>
2021-09-17 16:34:03 +0900

    確認用コミット　Lesson7 Chapter8 まで

---
+++ app/src/main/java/jp/techacademy/naomasa/sakamoto/apiapp/ApiApplication.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.naomasa.sakamoto.apiapp
+
+import android.app.Application
+import io.realm.Realm
+
+class ApiApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
